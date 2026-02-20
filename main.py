cf0c5861a11dac1d9f90a02f98ba8e9b1f30c02a
Alex Ivanov <axkviv24@gmail.com>
2026-02-20 13:36:23 +0000

    Some vague idea of a super late beginning, reading in train dataset

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import tensorflow as tf
+from keras.src.utils import image_dataset_from_directory
+from tensorflow import keras
+
+# read about __init__.py and __main__.py
+
+def read_data():
+     train_dataset = image_dataset_from_directory(
+         "datasets/Dataset/Train"
+     )
+     print(train_dataset)
+
 def main():
     print("Hello World!")
+    read_data()
 
 if __name__ == "__main__":
     main()
