95421287db43adcc6ad6fac87b35854e5bc797e9
Alex Ivanov <axkviv24@gmail.com>
2026-02-23 02:19:17 +0000

    Added batch size and image size, reads in all (current) directories

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ from tensorflow import keras
 
 # read about __init__.py and __main__.py
 
-def read_data():
+# Resizing input images to 299 x 299, default input size for Xception (CNN)
+IMG_SIZE = (299, 299)
+# Number of images trained at once
+BATCH_SIZE = 32
+
+def read_data(directory):
      train_dataset = image_dataset_from_directory(
-         "datasets/Dataset/Train"
+         directory,
+         image_size=IMG_SIZE,
+         batch_size=BATCH_SIZE,
+         label_mode="binary",
+         # Potentially add -> shuffle=True
      )
-     print(train_dataset)
+     print(type(train_dataset))
 
 def main():
     print("Hello World!")
-    read_data()
+    read_data("datasets/Dataset/Train")
+    read_data("datasets/Dataset/Test")
+    read_data("datasets/Dataset/Validation")
 
 if __name__ == "__main__":
     main()
